a15102f9d47b284addbc7c1a29253b6bd4223fd9
pacafs <paca.fs@gmail.com>
2016-03-11 11:56:22 +0000

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ end
 
 puts "Success!!"
 
+(1..3).each_with_index do |u, index|
+	user = User.create! :username => 'User' + index.to_s, :email => index.to_s + "@gmail.com", :password => "password", :password_confirmation => "password", :avatar => "http://www.iconarchive.com/download/i59027/hopstarter/superhero-avatar/Avengers-Nick-Fury.ico"
+end
+
+
+
